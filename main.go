729de3403864e042248129d65c8766ef0f7ef77f
aladine <dan.aladine@gmail.com>
2015-10-15 20:57:29 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,51 @@
+package dictutil
+
+import (
+	"flag"
+	log "github.com/Sirupsen/logrus"
+	"io/ioutil"
+  "os"
+)
+
+var (
+	token_file = flag.String("token", "", "File contains token")
+)
+
+func GetToken() string {
+	flag.Parse()
+	if *token_file == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	token, err := ioutil.ReadFile(*token_file)
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+	return string(token)
+}
+
+func LogInit() {
+	log.SetFormatter(&log.JSONFormatter{})
+}
+
+func LogInfo(e string, t int, info interface{}) {
+	log.WithFields(log.Fields{
+		"event": e,
+		"user":  t,
+	}).Info(info)
+}
+
+func LogWarn(e string, t int, info interface{}) {
+	log.WithFields(log.Fields{
+		"event": e,
+		"user":  t,
+	}).Warn(info)
+}
+
+func LogError(e string, t int, info interface{}) {
+	log.WithFields(log.Fields{
+		"event": e,
+		"user":  t,
+	}).Error(info)
+}
