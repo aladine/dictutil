4a14801ae28ae6c55e4532371e8ceee78b5068a0
dan <dan.aladine@gmail.com>
2015-11-08 23:46:43 +0800

    add dictionary struct

--- main.go
+++ main.go
@@ -11,6 +11,82 @@ var (
 	token_file = flag.String("token", "", "File contains token")
 )
 
+type dictionary struct {
+	base     string
+	word_idx map[string]string
+}
+
+//Usage:
+// d:= dictionary{base:"",make(map[string]string)}
+
+func (d *dictionary) PrepareIndex() {
+	// read  dictionary
+	idx_data, err := ioutil.ReadFile(base + ".idx")
+	if err != nil {
+		fmt.Printf("Failed to open the dictionary: %s\n", err.Error())
+	}
+
+	dict_data, err := os.Open(base + ".dict")
+	if err != nil {
+		fmt.Printf("Failed to open the dictionary: %s\n", err.Error())
+	}
+
+	// close fi on exit and check for its returned error
+	defer func() {
+		if err := dict_data.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	reader := bytes.NewBuffer(idx_data)
+
+	for {
+		word, err := reader.ReadString('\x00')
+		word = strings.TrimRight(word, "\x00")
+
+		if err != nil {
+			break
+		}
+
+		// offset
+		offset, err := GetNumber(reader)
+		if err != nil {
+			break
+		}
+
+		// length
+		length, err := GetNumber(reader)
+		if err != nil {
+			break
+		}
+
+		// desc
+		desc := make([]byte, length)
+		dict_data.ReadAt(desc, int64(offset))
+		d.word_idx[word] = string(desc)
+	}
+}
+
+func (d *dictionary) Check(word string) (string, error) {
+	desc, exists := d.word_idx[word]
+	if !exists {
+		return "", errors.New("notFound")
+	}
+	return desc, nil
+}
+
+func GetNumber(b *bytes.Buffer) (int32, error) {
+	var length int32
+	b_length := make([]byte, 4)
+	if n, err := b.Read(b_length); err != nil || n != 4 {
+		fmt.Println("length err", n, err)
+		return 0, errors.New("length err")
+	}
+	binary.Read(bytes.NewBuffer(b_length), binary.BigEndian,
+		&length)
+	return length, nil
+}
+
 func GetToken() string {
 	flag.Parse()
 	if *token_file == "" {
@@ -24,9 +100,6 @@ func GetToken() string {
 	}
 	return string(token)
 }
-
-//func PrepareIndex(base string) {}
-//func Check(word string) {}
 
 func LogInit() {
 	log.SetFormatter(&log.JSONFormatter{})
